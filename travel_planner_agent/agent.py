8f78b67077a5574b7c2ffc6d6de164a59298bbac
GA <appeagyeigeorgina@gmail.com>
2026-04-26 15:58:52 +0100

    one simple agent

--- travel_planner_agent/agent.py
+++ travel_planner_agent/agent.py
@@ -1,8 +1,13 @@
-from google.adk.agents.llm_agent import Agent
+from google.adk.agents import Agent
+from google.adk.tools import google_search
 
 root_agent = Agent(
-    model='gemini-2.5-flash',
-    name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    name="travel_agent",
+    model="gemini-2.5-flash",
+    tools=[google_search],
+    instruction="""You are a travel agent.
+Your job is to help the user plan a trip.
+You have access to a search engine.
+If you don't know the answer, you can use the search engine.
+When you are done, reply with "DONE".""",
 )
